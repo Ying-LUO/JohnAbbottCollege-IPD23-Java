e94513af0ec37daa20fd3afbaa23a13278323d2c
Ying LUO <ying.luo@johnabbottcollege.net>
2020-11-24 21:01:50 -0500

    new pro

--- Day09FirstSQL/src/day09firstsql/Day09FirstSQL.java
+++ Day09FirstSQL/src/day09firstsql/Day09FirstSQL.java
@@ -5,6 +5,10 @@
  */
 package day09firstsql;
 
+
+import java.sql.*;
+import java.util.Scanner;
+
 /**
  *
  * @author YingLuo
@@ -14,8 +18,112 @@ public class Day09FirstSQL {
     /**
      * @param args the command line arguments
      */
+    
+    static Scanner input = new Scanner(System.in);
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        try{
+            
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ipd23people", "root", "root");
+
+            
+            //INSERT
+            {
+                System.out.print("Enter Name: ");
+                String name = input.nextLine();
+                System.out.print("Enter Age: ");
+                int age = input.nextInt();
+            
+                String sql = "INSERT INTO people (name, age) VALUES (?, ?)";
+
+                PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+                statement.setString(1, name);
+                statement.setInt(2, age);
+
+                statement.executeUpdate();  //SQLException
+
+                //obtain ID of the newly inserted record if you need to
+                ResultSet rs = statement.getGeneratedKeys();
+                if(rs.next()){
+                    int lastInsertedId = rs.getInt(1);
+                    System.out.println("Inserted new record with id=" + lastInsertedId);
+                }
+            }
+            
+            
+            //SELECT
+            {
+                String sql = "SELECT * FROM people";
+
+                Statement statement = conn.createStatement();
+                ResultSet result = statement.executeQuery(sql); //SQLException
+
+                int count = 0;
+
+                while (result.next()){
+                    
+                    int id = result.getInt("id");
+                    
+                    String name = result.getString("name");
+                    int age = result.getInt("age");
+                    
+                    System.out.printf("%nPerson: id=%d, name=%s, age=%d", id, name, age);
+                }
+            }
+            
+            //UPDATE
+            {
+                System.out.print("\n\nEnter id of the record to update: ");
+                int id = input.nextInt();
+                input.nextLine();  // consume the left-over newline, add this after every nextInt
+                System.out.print("Enter Name: ");
+                String name = input.nextLine();
+                
+                System.out.print("Enter Age: ");
+                int age = input.nextInt();
+                input.nextLine();  // consume the left-over newline, add this after every nextInt
+            
+                String sql = "UPDATE people SET name=?, age=? WHERE id=?";
+ 
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setString(1, name);
+                statement.setInt(2, age);
+                statement.setInt(3, id);
+
+                statement.executeUpdate();
+                System.out.println("Person updates successfully");
+                
+            }
+            
+            //DELETE
+            {
+                System.out.println("\nEnter the id you want to delete: ");
+                int id = input.nextInt();
+                input.nextLine();  // consume the left-over newline, add this after every nextInt
+                
+                String sql = "DELETE FROM people WHERE id=?";
+ 
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setInt(1, id);
+
+                int rowsDeleted = statement.executeUpdate();
+                if (rowsDeleted > 0) {
+                    System.out.println("A record of people was deleted successfully!");
+                }else{
+                    System.out.println("no such record found - none deleted");
+                }
+
+            }
+ 
+        } catch(SQLException ex){
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();   // now show to the user, but show in console
+        }
+        
+        
     }
     
 }
---
+++ Day09PeopleDB/src/day09peopledb/Database.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day09peopledb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author YingLuo
+ */
+public class Database {
+    
+    // move it outside from the constructor to be a field
+    Connection conn;
+            
+            
+    public Database() throws SQLException{
+        
+        //if define here which is local variable that cannot be used for others
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ipd23people", "root", "ZAQ12wsxly");
+
+        
+    }
+    
+    // database lier which exception may happens
+    public ArrayList<Person> getAllPeople() throws SQLException{
+        
+        ArrayList<Person> resultList = new ArrayList<>();
+        
+        String sql = "SELECT * FROM people";
+
+        Statement statement = conn.createStatement();
+        ResultSet result = statement.executeQuery(sql); //SQLException
+
+        while (result.next()){
+
+            int id = result.getInt("id");
+
+            String name = result.getString("name");
+            int age = result.getInt("age");
+
+            resultList.add(new Person(id, name, age));
+        }
+        
+        return resultList;
+    }
+    
+    // return new inserted record id
+    public int addPerson(Person p) throws SQLException{
+        
+        
+        String sql = "INSERT INTO people (name, age) VALUES (?, ?)";
+
+        PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+        statement.setString(1, p.getName());
+        statement.setInt(2, p.getAge());
+
+        statement.executeUpdate();  //SQLException
+
+        //obtain ID of the newly inserted record if you need to
+        ResultSet rs = statement.getGeneratedKeys();
+        
+        if(rs.next()){
+            int lastInsertedId = rs.getInt(1);
+            return lastInsertedId;
+        }
+        //return -1;  // not good choice without any documentation
+        throw new SQLException("Id after insert not found");
+    }
+    
+    public void updatePerson(Person p) throws SQLException{
+
+        String sql = "UPDATE people SET name=?, age=? WHERE id=?";
+
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.setString(1, p.getName());
+        statement.setInt(2, p.getAge());
+        statement.setInt(3, p.getId());
+
+        statement.executeUpdate();
+       
+    }
+    
+    public int deletePerson(int id) throws SQLException{
+        
+        String sql = "DELETE FROM people WHERE id=?";
+ 
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.setInt(1, id);
+
+        return statement.executeUpdate();
+        
+    }
+    
+}
